a4054015e87826f2ed95a66257ea318ccfe2b554
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-07-24 20:11:24 +0100

    Remove duplicate parameter entries

--- R/parameters.R
+++ R/parameters.R
@@ -1,10 +1,10 @@
 
 #' @title Useful default parameters
-#' @param population An object of class `<population>` from the \pkg{epidemics}
+#' @param population An optional argument taking an object of class
+#' `<population>` from the \pkg{epidemics}. Used to obtain a contact matrix.
 #' package.
 #' @export
 default_parameters <- function(population) {
-  checkmate::assert_class(population, "population")
   params <- list(
     contacts = matrix(1),
     sigma = 0.72,
@@ -14,16 +14,15 @@ default_parameters <- function(population) {
     D_immun = 6.8,
     probT_under5 = 1.8,
     probT_over5 = 3.6,
-    rho = 0.05,
     b = (11.4 / 1000) / 365,
     d = (11.4 / 1000) / 365,
-    sigma = 0.5,
     epsilon = 1,
     psi = 1 / 2,
     gamma = 1 / 10,
     n_age_groups = 4
   )
   if (!missing(population)) {
+    checkmate::assert_class(population, "population")
     contacts <- population$contact_matrix / population$demography_vector
     params[["contacts"]] <- contacts
   }
