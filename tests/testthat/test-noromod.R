c53c1d9003bc17071ff7a3015e2a6f8495f032f5
pratikunterwegs <pratikgupte16@gmail.com>
2024-02-14 10:21:15 +0000

    Use Readme example in tests, add Boost equivalence check

--- tests/testthat/test-noromod.R
+++ tests/testthat/test-noromod.R
@@ -1,68 +1,92 @@
 #### Basic tests for equivalence and correctness ####
 
-test_that("Basic expectations", {
-  # seven compartments
-  uk_pop <- epidemics::population(
-    name = "UK population",
-    contact_matrix = matrix(1, 4, 4),
-    demography_vector = 67e6 * rep(0.25, 4),
-    initial_conditions = matrix(
-      c(1 - 1e-6, 0, 1e-6, 0, 0, 0, 0),
-      nrow = 4, ncol = 7, byrow = TRUE
-    )
-  )
+# prepare initial conditions from example in Readme
+init <- c(
+  3857263, 8103718, 42460865, 12374961,
+  100, 0, 0, 0,
+  0, 0, 0, 0,
+  0, 0, 0, 0,
+  0, 0, 0, 0,
+  0, 0, 0, 0,
+  0, 0, 0, 0
+)
+init_mat <- matrix(
+  init,
+  nrow = 4, ncol = 7
+)
 
+age_groups <- c(0, 5, 15, 65)
+polymod <- socialmixr::polymod
+UK_structure <- socialmixr::contact_matrix(
+  polymod,
+  countries = "United Kingdom",
+  age.limits = c(age_groups),
+  symmetric = TRUE
+)
+
+# Symmetrical contact matrix
+uk_contact_rate_matrix <- as.matrix(UK_structure$matrix)
+demography <- UK_structure$demography$population
+
+uk_contact_rate_matrix <- t(t(uk_contact_rate_matrix) / demography)
+
+# add contact matrix to pop
+params <- default_parameters()
+params[["contacts"]] <- uk_contact_rate_matrix
+
+test_that("Basic expectations for R and Rcpp ODE systems for deSolve", {
+  # pick a random timestep
   tstep <- sample(seq(100), 1)
   noromod_r <- norovirus_model_r(
     t = tstep,
-    state = uk_pop$initial_conditions * uk_pop$demography_vector,
-    default_parameters(population = uk_pop)
+    state = init_mat,
+    parameters = params
   )
 
   noromod_cpp <- norovirus_model_cpp(
     t = tstep,
-    state = uk_pop$initial_conditions * uk_pop$demography_vector,
-    default_parameters(population = uk_pop)
+    state = init_mat,
+    parameters = params
   )
 
-  # expect integration with lsoda
-  times <- seq(1000)
-  expect_no_condition(
-    deSolve::lsoda(
-      y = uk_pop$initial_conditions * uk_pop$demography_vector,
-      times = times,
-      func = norovirus_model_r,
-      parms = default_parameters(population = uk_pop)
-    )
-  )
-  expect_no_condition(
-    deSolve::lsoda(
-      y = uk_pop$initial_conditions * uk_pop$demography_vector,
-      times = times,
-      func = norovirus_model_cpp,
-      parms = default_parameters(population = uk_pop)
-    )
-  )
-
-  # expect identical
+  # expect equivalence for a single timestep
   expect_identical(
     noromod_cpp, noromod_r,
     tolerance = 1e-6
   )
 
+  # expect identical integration with deSolve::lsoda
+  times <- seq(1000)
+  expect_no_condition(
+    deSolve::lsoda(
+      y = init_mat,
+      times = times,
+      func = norovirus_model_r,
+      parms = params
+    )
+  )
+  expect_no_condition(
+    deSolve::lsoda(
+      y = init_mat,
+      times = times,
+      func = norovirus_model_cpp,
+      parms = params
+    )
+  )
+
   # expect values over time are identical (allow greater tolerance)
   expect_identical(
     deSolve::lsoda(
-      y = uk_pop$initial_conditions * uk_pop$demography_vector,
+      y = init_mat,
       times = times,
       func = norovirus_model_cpp,
-      parms = default_parameters(population = uk_pop)
+      parms = params
     ),
     deSolve::lsoda(
-      y = uk_pop$initial_conditions * uk_pop$demography_vector,
+      y = init_mat,
       times = times,
       func = norovirus_model_r,
-      parms = default_parameters(population = uk_pop)
+      parms = params
     ),
     tolerance = 1
   )
@@ -75,82 +99,71 @@ test_that("Basic expectations", {
   expect_snapshot(
     norovirus_model_r(
       t = 100,
-      state = uk_pop$initial_conditions * uk_pop$demography_vector,
-      default_parameters(population = uk_pop)
+      state = init_mat,
+      parameters = params
     )
   )
   expect_snapshot(
     norovirus_model_cpp(
       t = 100,
-      state = uk_pop$initial_conditions * uk_pop$demography_vector,
-      default_parameters(population = uk_pop)
+      state = init_mat,
+      parameters = params
     )
   )
 })
 
-test_that("Model with Boost C++ solvers", {
-  init <- c(
-    3857263, 8103718, 42460865, 12374961,
-    100, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0
-  )
-  init_mat <- matrix(
-    init,
-    nrow = 4, ncol = 7
-  )
-
-  age_groups <- c(0, 5, 15, 65)
-  polymod <- socialmixr::polymod
-  UK_structure <- socialmixr::contact_matrix(
-    polymod,
-    countries = "United Kingdom",
-    age.limits = c(age_groups),
-    symmetric = TRUE
-  )
-
-  # Symmetrical contact matrix
-  uk_contact_rate_matrix <- as.matrix(UK_structure$matrix)
-  demography <- UK_structure$demography$population
-
-  uk_contact_rate_matrix <- t(t(uk_contact_rate_matrix) / demography)
-
-  # seven compartments
-  uk_pop <- epidemics::population(
-    name = "UK population",
-    contact_matrix = uk_contact_rate_matrix,
-    demography_vector = demography,
-    initial_conditions = matrix(
-      c(1 - 1e-6, 0, 1e-6, 0, 0, 0, 0),
-      nrow = 4, ncol = 7, byrow = TRUE
+test_that("Expectations and equivalence for Boost C++ ODE system", {
+  # run model and expect no conditions for a short run
+  time_end <- 1100
+  expect_no_condition(
+    noromod_cpp_boost(
+      initial_conditions = init_mat,
+      params = params, time_end = time_end, increment = 1
     )
   )
-
-  # add contact matrix to pop
-  params <- default_parameters(uk_pop)
-  params[["contacts"]] <- uk_contact_rate_matrix
-
-  # run model
   data <- noromod_cpp_boost(
-    initial_conditions = matrix(init, nrow = 4, ncol = 7),
+    initial_conditions = init_mat,
     params = params, time_end = 1100, increment = 1
   )
 
   # expect output is a list
   expect_vector(data, list())
-  # expect dataframes
-  data <- output_to_df(data)
-  expect_s3_class(data, "data.frame")
 
-  # expect all numeric and greater than 0
+  # expect all values numeric and greater than 0
   expect_true(
     all(
-      apply(data, 2, function(x) {
+      vapply(data[["x"]], FUN = function(x) {
         all(is.numeric(x)) && all(x >= 0)
-      })
+      }, FUN.VALUE = TRUE)
     )
+  )
+
+  # expect equivalence with R model
+  data_r <- deSolve::lsoda(
+    y = init_mat,
+    times = seq(0, time_end), # add zero to times
+    func = norovirus_model_r,
+    parms = params
+  )
+
+  # expect times are identical
+  expect_identical(
+    data[["time"]],
+    data_r[, "time"]
+  )
+
+  # expect values are identical
+  # convert output to comparable form
+  data_as_matrix <- matrix(data[["time"]], nrow = length(data[["time"]]))
+  data_as_matrix <- cbind(
+    data_as_matrix, do.call(rbind, lapply(data[["x"]], as.vector))
+  )
+
+  # expect equal with high tolerance for solver differences
+  expect_equal(
+    data_as_matrix[1:10, 1:10],
+    data_r[1:10, 1:10],
+    ignore_attr = TRUE,
+    tolerance = 1
   )
 })
