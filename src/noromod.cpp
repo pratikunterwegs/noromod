47f2cd6ca6efd23718208787c184da9d4df510b8
pratikunterwegs <pratikgupte16@gmail.com>
2024-05-20 13:54:24 +0100

    Update `noromod_cpp_boost()` with Eigen Tensors

--- src/noromod.cpp
+++ src/noromod.cpp
@@ -4,6 +4,7 @@
 #include <Rcpp.h>
 #include <RcppEigen.h>
 
+#include <unsupported/Eigen/CXX11/Tensor>
 #include <cmath>
 #include <vector>
 #include <boost/numeric/odeint.hpp>
@@ -15,7 +16,7 @@
 // [[Rcpp::depends(RcppEigen)]]
 // [[Rcpp::depends(BH)]]
 
-typedef std::vector<double> state_type;
+typedef Eigen::VectorXd state_type;
 
 /// @brief A simple observer for the integrator
 struct observer {
@@ -244,14 +245,17 @@ Rcpp::List norovirus_model_cpp(const double &t,
 }
 
 struct norovirus_model {
-  const double rho, b, births, d;
-  Eigen::Tensor<double, 1> d_rate; // remove background mortality
-  Rcpp::NumericVector sigma;
-  Eigen::Tensor<double, 2> sigma_tensor (sigma.size(), sigma.size());
-  const double epsilon, psi, gamma, phi, upsilon;
-  double delta, w1, q1, q2, seasonal_term;
+  const double rho, b, d;
+  double births = 0.0;
+  Eigen::Tensor<double, 1> d_rate;  // remove background mortality
+  Rcpp::NumericVector sigma;        // has size 3L for 3 levels of protection
+  Eigen::Tensor<double, 2> sigma_tensor =
+      Eigen::Tensor<double, 2>(3, 3);  // for 3 levels of protection
+  const double epsilon, psi, gamma;
+  double delta, w1, q1, q2;
+  double seasonal_term = 0.0;
   const std::vector<double> w2_values, season_change_points;
-  Rcpp::NumericMatrix contacts, aging;
+  std::vector<double> contacts, aging;  // cannot be const as mapped to Tensor
   Rcpp::NumericVector phi_1, phi_2, upsilon_1, upsilon_2;
   Eigen::Tensor<double, 2> contacts_tensor, aging_tensor;
   Eigen::Tensor<double, 1> param_;
@@ -272,12 +276,11 @@ struct norovirus_model {
         phi_2(params["phi_2"]),
         upsilon_1(params["upsilon_1"]),
         upsilon_2(params["upsilon_2"]),
-        q2(params["probT_over5"]),
-        w2_values(Rcpp::as<std::vector<double> >(params["season_offset"])),
+        w2_values(Rcpp::as<std::vector<double>>(params["season_offset"])),
         season_change_points(
-            Rcpp::as<std::vector<double> >(params["season_change_points"])),
-        contacts(params["contacts"]),
-        aging(Rcpp::as<Eigen::MatrixXd>(params["aging"])) {}
+            Rcpp::as<std::vector<double>>(params["season_change_points"])),
+        contacts(Rcpp::as<std::vector<double>>(params["contacts"])),
+        aging(Rcpp::as<std::vector<double>>(params["aging"])) {}
 
   void init_model() {
     // parameters
@@ -290,34 +293,35 @@ struct norovirus_model {
     // param Tensor
     param_.setValues({q1, q2, q2, q2});
 
-    // vaccination Tensor
-
-    // waning Tensor
+    // TODO: vaccination and waning
 
     // Sigma Tensor initialisation, create a diagonal matrix
     sigma_tensor.setZero();
-    for (size_t i = 0; i < sigma.size(); i++)
-    {
+    for (size_t i = 0; i < sigma.size(); i++) {
       sigma_tensor(i, i) = sigma[i];
     }
-    
 
-    // map to tensors; number of age groups is hardcoded to 4 giving 4x4 matrices
-    contacts_tensor = Eigen::TensorMap<Eigen::Tensor<double, 2>> (contacts, 4, 4);
-    aging_tensor = Eigen::TensorMap<Eigen::Tensor<double, 2>> (aging, 4, 4);
+    // map to tensors; number of age groups is hardcoded to 4 giving 4x4
+    // matrices
+    contacts_tensor =
+        Eigen::TensorMap<Eigen::Tensor<double, 2, Eigen::ColMajor>>(
+            &contacts[0], 4, 4);
+    aging_tensor = Eigen::TensorMap<Eigen::Tensor<double, 2, Eigen::ColMajor>>(
+        &aging[0], 4, 4);
   }
 
   // add nolint flags to allow passing by reference
-  void operator()(std::vector<double> &x, // NOLINT
-                  std::vector<double> &dx,  // NOLINT
+  void operator()(state_type &x,   // NOLINT
+                  state_type &dx,  // NOLINT
                   const double t) {
-
     // map a tensor to the state vector with required dims (4, 7, 3)
     // for age groups, epi compartments, vaccination strata
-    auto x_tensor = Eigen::TensorMap<Eigen::Tensor<double, 3, Eigen::ColMajor>> 
-      (&x[0], 4, 7, 3);
-    auto dx_tensor = Eigen::TensorMap<Eigen::Tensor<double, 3, Eigen::ColMajor>>
-      (&dx[0], 4, 7, 3)
+    auto x_tensor = Eigen::TensorMap<Eigen::Tensor<double, 3, Eigen::ColMajor>>(
+        &x[0], 4, 7, 3);
+    auto dx_tensor =
+        Eigen::TensorMap<Eigen::Tensor<double, 3, Eigen::ColMajor>>(&dx[0], 4,
+                                                                    7, 3);
+    dx_tensor.setZero();
 
     // prepare w2_current, initially first value
     double w2_current = w2_values[0] / 100.0;
@@ -332,63 +336,78 @@ struct norovirus_model {
     // recalculate seasonal forcing term
     seasonal_term = seasonal_forcing(t, w1, w2_current);
 
-    // NB: Casting initial conditions matrix columns to arrays is necessary
-    // for vectorised operations
+    // prepare matrix product dimensions
+    std::array<Eigen::IndexPair<int>, 1> product_dims = {
+        Eigen::IndexPair<int>(1, 0)};
 
     // column indices: 0:S, 1:E, 2:Is, 3:Ia, 4:R, 5:new_infections, 6:re_infects
     // calculate infection potential
-    auto infection_potential = contacts_tensor.contract(
-      (x_tensor.chip(2, 1) + (x_tensor.chip(3, 1) * rho)).sum(Eigen::array<int, 1>({1})),
-      Eigen::IndexPair<int>(1, 0)
-    ) * seasonal_term;
-    
+    // a 1D tensor of group-wise infections in all strata
+    // reshape to two dimensions: 4 x 1
+    auto groupwise_infected =
+        (x_tensor.chip(2, 1) + (x_tensor.chip(3, 1) * rho))  // dims: 4 x 3
+            .sum(std::array<int, 1>({1}))                    // dims: 4
+            .reshape(std::array<int, 2>{4, 1});              // dims: 4 x 1
+
+    // matrix multiply contacts x groupwise infected; result is 4 x 1
+    // multiply with season coeff
+    auto infection_potential =
+        contacts_tensor.contract(groupwise_infected, product_dims) *
+        seasonal_term;  // dims: 4 x 1
+
     // calculate new infections and reinfections in each vaccination stratum
-    auto new_infections = x_tensor.chip(0, 1) * infection_potential.broadcast(std::array<long, 1>{3}).reshape(std::array<long, 2>{4, 3});
-    auto re_infections = x_tensor.chip(4, 1) * infection_potential.broadcast(std::array<long, 1>{3}).reshape(std::array<long, 2>{4, 3});
+    // dims are 4 * 3 for both
+    auto new_infections = x_tensor.chip(0, 1) * infection_potential.broadcast(
+                                                    std::array<int, 2>{1, 3});
+    auto re_infections = x_tensor.chip(4, 1) * infection_potential.broadcast(
+                                                   std::array<int, 2>{1, 3});
 
     // calculate births as b * total population; b = mean per-capita births
     // define starting offset, i.e., starting at 1st row, col, stratum
-    Eigen::array<Eigen::Index, 3> offsets = {0, 0, 0};
+    std::array<Eigen::Index, 3> offsets = {0, 0, 0};
     // all four ages, first five epi compartments, all vax strata
-    Eigen::array<Eigen::Index, 3> extents = {4, 5, 3};
+    std::array<Eigen::Index, 3> extents = {4, 5, 3};
     // births is a double for the total number of births, while `b` is the rate
-    births = x_tensor.slice(offsets, extents).sum(Eigen::array<int, 3>({0, 1, 2})) * b;
+    Eigen::Tensor<double, 0> births_temp =
+        x_tensor.slice(offsets, extents).sum() * b;
+    births = births_temp.coeff();
 
     // changes in susceptibles from births and infections
-    dx_tensor.chip(0, 1) = -new_infections + (delta * x_tensor.chip(4, 0));
+    Eigen::Tensor<double, 2> recovery_waning = (delta * x_tensor.chip(4, 0));
+    dx_tensor.chip(0, 1) = recovery_waning - new_infections;
     // births enter the unvaccinated susceptible, lowest age group only
-    dx_tensor(0, 0, 0) = dx_tensor(0, 0, 0) + births; 
+    dx_tensor(0, 0, 0) = dx_tensor(0, 0, 0) + births;
 
     // changes in exposed from infections
-    dx_tensor.chip(1, 1) = new_infections - (epsilon * x_tensor.chip(1, 1));
-
-    // define product dims
-    Eigen::array<Eigen::IndexPair<int>, 1> product_dims = {
-      Eigen::IndexPair<int>(1, 0)
-    };
+    dx_tensor.chip(1, 1) = -(epsilon * x_tensor.chip(1, 1)) + new_infections;
 
     // changes in infectious asymptomatic
-    dx_tensor.chip(2, 1) = (-psi * x_tensor.chip(2, 1)) + 
-      (epsilon * x_tensor.chip(2, 1).contract(sigma_tensor, product_dims));
+    dx_tensor.chip(2, 1) =
+        (-psi * x_tensor.chip(2, 1)) +
+        (epsilon * x_tensor.chip(2, 1).contract(sigma_tensor, product_dims));
 
     // changes in infectious symptomatic
-    dx_tensor.chip(3, 1) = (psi * x_tensor.chip(2, 1)) - (gamma * x_tensor.chip(3, 1)) +
-      re_infections +
-      (epsilon * x_tensor.chip(2, 1).contract(1.0 - sigma_tensor, product_dims));
+    dx_tensor.chip(3, 1) =
+        (psi * x_tensor.chip(2, 1)) - (gamma * x_tensor.chip(3, 1)) +
+        (epsilon *
+         x_tensor.chip(2, 1).contract(1.0 - sigma_tensor, product_dims));
+    dx_tensor.chip(3, 1) = dx_tensor.chip(3, 1) + re_infections;
 
     // changes in recoveries
     dx_tensor.chip(4, 1) = (gamma * x_tensor.chip(3, 1)) - re_infections;
 
-    // calculate background mortality in all epi compartments; uniform mortality rate
-    dx_tensor.slice(offsets, extents) = dx_tensor.slice(offsets, extents) - (x_tensor.slice(offsets, extents) * d);
+    // calculate background mortality in all epi compartments; uniform mortality
+    // rate
+    dx_tensor.slice(offsets, extents) = dx_tensor.slice(offsets, extents) -
+                                        (x_tensor.slice(offsets, extents) * d);
 
     // apply aging-related flows to all compartments and all vaccination strata
     // NOTE: dx = x + aging; aging matrix coeffs have appropriate signs
-    for (size_t i = 0; i < 3L; i++)
-    {
-      dx_tensor.slice(offsets, extents) = dx_tensor.slice(offsets, extents) + (
-        aging_tensor.contract(x_tensor.slice(offsets, extents), product_dims);
-      )
+    for (size_t i = 0; i < 3L; i++) {
+      dx_tensor.slice(offsets, extents) =
+          dx_tensor.slice(offsets, extents) +
+          (aging_tensor.contract(x_tensor.slice(offsets, extents),
+                                 product_dims));
     }
 
     // new infections
@@ -419,30 +438,34 @@ struct norovirus_model {
 //' @export
 // [[Rcpp::export(name="noromod_cpp_boost")]]
 Rcpp::List noromod_cpp_boost(
-    const Eigen::MatrixXd &initial_conditions, const Rcpp::List &params,
+    Eigen::VectorXd &initial_conditions,  // NOLINT
+    const Rcpp::List &params,
     const double &time_end = 200.0,  // double required by boost solver
     const double &increment = 1.0) {
   // initial conditions from input
-  odetools::state_type x = initial_conditions;
+  state_type x = initial_conditions;
+
+  // map to an Eigen Tensor
+  auto state3d = Eigen::TensorMap<Eigen::Tensor<double, 3, Eigen::ColMajor>>(
+      &initial_conditions[0], 2, 2, 3);
 
   // create a default epidemic with parameters
   norovirus_model this_model(params);
   this_model.init_model();
 
   // prepare storage containers for the observer
-  std::vector<odetools::state_type> x_vec;  // is a vector of MatrixXd
+  std::vector<state_type> x_vec;  // is a vector of vectors
   std::vector<double> times;
 
   // a controlled stepper for constant step sizes
   boost::numeric::odeint::runge_kutta4<
-      odetools::state_type, double, odetools::state_type, double,
+      state_type, double, state_type, double,
       boost::numeric::odeint::vector_space_algebra>
       stepper;
 
   // run the function without assignment
   boost::numeric::odeint::integrate_const(stepper, this_model, x, 0.0, time_end,
-                                          increment,
-                                          odetools::observer(x_vec, times));
+                                          increment, observer(x_vec, times));
 
   return Rcpp::List::create(Rcpp::Named("x") = Rcpp::wrap(x_vec),
                             Rcpp::Named("time") = Rcpp::wrap(times));
