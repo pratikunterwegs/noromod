4b4d77677ce832b686fe6e64b7eb7647b4a65222
bolthikaru <bolt.hikaru@gmail.com>
2024-11-02 11:30:23 +0000

    vaccination model update
    Updated so that number of individuals being vaccinated in each age group can be tracked. As those in the S & R compartment are vaccinated, trying to keep track of the numbers vaccinated from each of these groups.

--- R/noromod_r.R
+++ R/noromod_r.R
@@ -24,40 +24,50 @@ seasonal_forcing <- function(t, w1, w2) {
 norovirus_model_r <- function(t, state, parameters) {
   # prepare initial conditions
   n_age_groups <- parameters[["n_age_groups"]]
-
-  # compartmental structure
-  # S|E|Ia|Is|R|new_infections|re_infections
-  # V1|Ev1|Iav1|Isv1|Rv1|new_infections_v1|reinfections_v1
-  # V2|Ev2|Iav2|Isv2|Rv2|new_infections_v2|reinfections_v2
-
-  # hardcoding the number of columns
-  state <- matrix(
-    state,
-    nrow = n_age_groups, ncol = 21L
-  )
+  
+  # New structure (7 columns per section):
+  # Non-vaccinated: S|E|Is|Ia|R|re_infections|new_infections
+  # Vax1: V1|Ev1|Isv1|Iav1|Rv1|re_infections_v1|new_infections_v1
+  # Vax2: V2|Ev2|Isv2|Iav2|Rv2|re_infections_v2|new_infections_v2
+  # Plus 2 vaccination tracking columns at the end: new_vaccinations_v1|new_vaccinations_v2
+  
+  state <- matrix(state, nrow = n_age_groups, ncol = 23L)  # 7 + 7 + 7 + 2 = 23 columns
+  
+  # Non-vaccinated compartments (columns 1-7)
   susceptible <- state[, 1]
   exposed <- state[, 2]
   infect_symp <- state[, 3]
   infect_asymp <- state[, 4]
   recovered <- state[, 5]
-
+  re_infections_track <- state[, 6]
+  new_infections_track <- state[, 7]
+  
+  # Vaccine 1 compartments (columns 8-14)
   vax_1 <- state[, 8]
   exposed_v1 <- state[, 9]
   infect_symp_v1 <- state[, 10]
   infect_asymp_v1 <- state[, 11]
   recovered_v1 <- state[, 12]
-
+  re_infections_v1_track <- state[, 13]
+  new_infections_v1_track <- state[, 14]
+  
+  # Vaccine 2 compartments (columns 15-21)
   vax_2 <- state[, 15]
   exposed_v2 <- state[, 16]
   infect_symp_v2 <- state[, 17]
   infect_asymp_v2 <- state[, 18]
   recovered_v2 <- state[, 19]
-
-  # count the total population as the sum of the first five columns
-  # hardcoding the number of epidemiological compartments - S,E,Is,Ia,R
+  re_infections_v2_track <- state[, 20]
+  new_infections_v2_track <- state[, 21]
+  
+  # Vaccination tracking (columns 22-23)
+  new_vaccinations_v1_track <- state[, 22]
+  new_vaccinations_v2_track <- state[, 23]
+  
+  # count the total population
   total_pop <- rowSums(state[, c(1:5, 8:12, 15:19)])
-
-  # some parameters
+  
+  # Parameters
   delta <- 1 / (parameters[["D_immun"]] * 365)
   w1 <- (parameters[["season_amp"]] / 100)
   w2_values <- (parameters[["season_offset"]] / 100)
@@ -65,10 +75,9 @@ norovirus_model_r <- function(t, state, parameters) {
   q1 <- exp(parameters[["probT_under5"]])
   q2 <- exp(parameters[["probT_over5"]])
   q <- c(q1, q2, q2, q2)
-
-  # more parameters
+  
   rho <- parameters[["rho"]]
-  b <- parameters[["b"]] #* 60286751
+  b <- parameters[["b"]]
   d <- parameters[["d"]]
   sigma <- parameters[["sigma"]][1]
   sigma_v1 <- parameters[["sigma"]][2]
@@ -78,15 +87,8 @@ norovirus_model_r <- function(t, state, parameters) {
   gamma <- parameters[["gamma"]]
   aging <- parameters[["aging"]]
   vacc_start <- parameters[["vacc_start"]]
-
-  # vaccination rates
-  # phi_1 <- parameters[["phi_1"]]
-  # phi_2 <- parameters[["phi_2"]]
-  # 
-  # # waning rates
-  # upsilon_1 <- parameters[["upsilon"]][1]
-  # upsilon_2 <- parameters[["upsilon"]][2]
-
+  
+  # Vaccination parameters
   if (t > vacc_start) {
     phi_1 <- parameters[["phi_1"]]
     phi_2 <- parameters[["phi_2"]]
@@ -99,55 +101,54 @@ norovirus_model_r <- function(t, state, parameters) {
     upsilon_2 <- 0
   }
   
-  # contact matrix
+  # Contact matrix
   cm <- parameters[["contacts"]]
-
+  
+  # Calculate seasonal term
   # calculate the current w2 using `season_offset_intervals` and `season_offset`
   # as a lookup table
   # use Position to search the list of intervals for the index where t is less
   # than the change point. Note default behaviour of Position() is to return the
   # first index that satisfies the condition
   # NOTE: t taken from function scope, this is the simulation time
+  
   w2_current <- w2_values[
     Position(
       f = function(x) t <= x, x = parameters[["season_change_points"]]
     )
   ]
-
   seasonal_term <- seasonal_forcing(t = t, w1 = w1, w2 = w2_current)
+  
+  # Calculate infection potentials
   infection_potential <- q * seasonal_term * (
     cm %*% (
       (infect_symp + infect_symp_v1 + infect_symp_v2) +
         (infect_asymp + infect_asymp_v1 + infect_asymp_v2) * rho
     )
   )
-
+  
   # apply seperate infection potential to the 4th age group
   infection_potential[4] <- infection_potential[4] * w3
   
-  
-  # calculate new infections by vax status
+  # Calculate all infections
   new_infections <- susceptible * infection_potential
   new_infections_v1 <- vax_1 * infection_potential
   new_infections_v2 <- vax_2 * infection_potential
-
-  # calculate re-infections by vax status
+  
   re_infections <- recovered * infection_potential
   re_infections_v1 <- recovered_v1 * infection_potential
   re_infections_v2 <- recovered_v2 * infection_potential
-
-  # Calculate births for addition only to the first age group
-  # TODO: reconsider how births are calculated. Consider `b * [14 - 65]` only?
-  # TODO: consider adding `b` to ageing matrix as inflow rate?
+  
+  # Calculate vaccinations
+  new_vaccinations_v1_S <- phi_1 * susceptible
+  new_vaccinations_v1_R <- phi_1 * recovered
+  
+  # Calculate births
   births <- c(b * sum(total_pop), rep(0, n_age_groups - 1))
-
-  # NOTE: see branch `array-vax` for implementations using 3D arrays in R & C++
-
-  # TODO: annotate transitions
-  # compartmental transitions - non vaccinated
-  dS <- births + (delta * recovered) - new_infections - (d * susceptible) +
-    (aging %*% susceptible) - (phi_1 * susceptible) + (upsilon_1 * vax_1) +
-    (recovered_v1 * (delta * upsilon_1))
+  
+  # Non-vaccinated compartment changes
+  dS <- births + (delta * recovered) - new_infections - (d * susceptible) + 
+    (aging %*% susceptible) - (phi_1 * susceptible) + (upsilon_1 * vax_1)
   dE <- new_infections - (epsilon * exposed) - (d * exposed) +
     (aging %*% exposed)
   dIs <- (epsilon * sigma * exposed) - (psi * infect_symp) - (d * infect_symp) +
@@ -156,13 +157,13 @@ norovirus_model_r <- function(t, state, parameters) {
     (gamma * infect_asymp) - (d * infect_asymp) + re_infections +
     (aging %*% infect_asymp)
   dR <- (gamma * infect_asymp) - (delta * recovered) - (d * recovered) -
-    re_infections + (aging %*% recovered) + (upsilon_1 * recovered_v1) -
-    (phi_1 * recovered)
-
-  # compartmental transitions - vax 1
-  dV1 <- (delta * recovered_v1) - new_infections_v1 - (d * vax_1) +
-    (aging %*% vax_1) - (phi_2 * vax_1) + (upsilon_2 * vax_2) +
-    (phi_1 * susceptible) - (upsilon_1 * vax_1) +
+    re_infections + (aging %*% recovered) - (phi_1 * recovered) + 
+    (upsilon_1 * recovered_v1)
+  
+  # Vaccine 1 compartment changes
+  dV1 <- (delta * recovered_v1) - new_infections_v1 - (d * vax_1) + 
+    (aging %*% vax_1) - (phi_2 * vax_1) + (upsilon_2 * vax_2) + 
+    (phi_1 * susceptible) - (upsilon_1 * vax_1) + 
     (recovered_v2 * (delta * upsilon_2))
   dEv1 <- new_infections_v1 - (epsilon * exposed_v1) -
     (d * exposed_v1) + (aging %*% exposed_v1)
@@ -171,13 +172,13 @@ norovirus_model_r <- function(t, state, parameters) {
   dIav1 <- (epsilon * (1 - sigma_v1) * exposed_v1) + (psi * infect_symp_v1) -
     (gamma * infect_asymp_v1) - (d * infect_asymp_v1) + re_infections_v1 +
     (aging %*% infect_asymp_v1)
-  dRv1 <- (gamma * infect_asymp_v1) - (delta * recovered_v1) -
-    (d * recovered_v1) - re_infections_v1 + (aging %*% recovered_v1) +
-    (upsilon_2 * recovered_v2) -
-    (phi_2 * recovered_v1) - (upsilon_1 * recovered_v1) + (phi_1 * recovered) -
-    (recovered_v1 * (delta * upsilon_1))
-
-  # compartmental transitions - vax 2
+  dRv1 <- (gamma * infect_asymp_v1) - (delta * recovered_v1) - 
+    (d * recovered_v1) - re_infections_v1 + (aging %*% recovered_v1) + 
+    (upsilon_2 * recovered_v2) - (phi_2 * recovered_v1) - 
+    (upsilon_1 * recovered_v1) + (phi_1 * recovered)
+  
+  
+  # Vaccine 2 compartment changes
   dV2 <- (delta * recovered_v2) - new_infections_v2 - (d * vax_2) +
     (aging %*% vax_2) + (phi_2 * vax_1) - (upsilon_2 * vax_2)
   dEv2 <- new_infections_v2 - (epsilon * exposed_v2) -
@@ -189,13 +190,25 @@ norovirus_model_r <- function(t, state, parameters) {
     (aging %*% infect_asymp_v2)
   dRv2 <- (gamma * infect_asymp_v2) - (delta * recovered_v2) -
     (d * recovered_v2) - re_infections_v2 + (aging %*% recovered_v2) -
-    (upsilon_2 * recovered_v2) +
-    (phi_2 * recovered_v1) -
+    (upsilon_2 * recovered_v2) + (phi_2 * recovered_v1) -
     (recovered_v2 * (delta * upsilon_2))
-
+  
+  
+  # Return exactly 92 values (23 columns × 4 rows)
   return(list(c(
-    dS, dE, dIs, dIa, dR, re_infections, new_infections,
-    dV1, dEv1, dIsv1, dIav1, dRv1, re_infections_v1, new_infections_v1,
-    dV2, dEv2, dIsv2, dIav2, dRv2, re_infections_v2, new_infections_v2
+    # Non-vaccinated compartments and tracking (7 values)
+    dS, dE, dIs, dIa, dR,
+    re_infections, new_infections,
+    
+    # Vaccine 1 compartments and tracking (7 values)
+    dV1, dEv1, dIsv1, dIav1, dRv1,
+    re_infections_v1, new_infections_v1,
+    
+    # Vaccine 2 compartments and tracking (7 values)
+    dV2, dEv2, dIsv2, dIav2, dRv2,
+    re_infections_v2, new_infections_v2,
+    
+    # Vaccination tracking (2 values)
+    new_vaccinations_v1_S, new_vaccinations_v1_R
   )))
 }
--- R/parameters.R
+++ R/parameters.R
@@ -9,24 +9,27 @@ default_parameters <- function() {
   aging[row(aging) - col(aging) == 1] <- 1 / utils::head(da, -1)
   # No ageing in last group - flow out via mortality rate
 
-  upsilon <- c(0, 0)
+  upsilon <- c(4.5, 4.5)
   upsilon <- 1 / (upsilon * 365)
   upsilon[is.infinite(upsilon)] <- 0.0
 
   params <- list(
+    target_coverage_under5 = 0.9,
+    target_coverage_over65 = 0.75,
+    days_to_target = 365,
     contacts = matrix(1),
-    sigma = c(0.82, 0.41, 0.41),
-    phi_1 = c(0, 0, 0, 0), # vector, one value per age group
+    sigma = c(0.82, 0.82/2, 0.82/2),
+    phi_1 = c(-log(1 - target_coverage_under5) / days_to_target,0,0,-log(1 - target_coverage_over65) / days_to_target), # vector, one value per age group
     phi_2 = c(0, 0, 0, 0),
     upsilon = upsilon,
     rho = 0.05,
-    season_amp = 3.6,
-    season_amp_over65 = 1.8,
-    season_offset = c(5.76, 0, 0, 0, 0, 0, 0),
+    season_amp = 3.9,
+    season_amp_over65 = 1.6,
+    season_offset = c(8.4),
     # NOTE: only need change points, i.e., final values of each season
     # season_change_points = c(8580, 8944, 9315, 9679, 10043, 10407, 10771),
     season_change_points = c(11000, 0, 0, 0, 0, 0, 0),
-    D_immun = 4.4,
+    D_immun = 9.01,
     probT_under5 = log(0.195),
     probT_over5 = log(0.039),
     b = (11.4 / 1000) / 365,
@@ -38,7 +41,7 @@ default_parameters <- function() {
     gamma = 1 / 10,
     n_age_groups = 4,
     aging = aging / 365,
-    vacc_start = 9000
+    vacc_start = 3535
   )
 
   params
